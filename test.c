13f1f9d1e0653e25ffce98fd6e8ed8491c9671a7
ysthakur <45539777+ysthakur@users.noreply.github.com>
2024-10-20 14:04:23 -0400

    Fix mark scan in test.c

--- test.c
+++ test.c
@@ -1,24 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum
+{
+  kBlack, // green
+  kGray, // red
+  kWhite // blue
+} Color;
+
 typedef struct Node_s
 {
   int rc;
+  Color color;
   int value;
   struct Node_s *left;
   struct Node_s *right;
 } *Node;
 
+void markGray(Node node) {
+  printf("[markGray]: Node: %d, rc: %d\n", node->value, node->rc);
+  if (node->color != kGray) {
+    node->color = kGray;
+    if (node->left) {
+      node->left->rc--;
+      markGray(node->left);
+    }
+    if (node->right) {
+      node->right->rc--;
+      markGray(node->right);
+    }
+  }
+}
+
+void scanBlack(Node node) {
+  printf("[scanBlack] Node: %d, rc: %d\n", node->value, node->rc);
+  if (node->color == kBlack) {
+    return;
+  }
+  node->color = kBlack;
+  if (node->left) {
+    node->left->rc++;
+    scanBlack(node->left);
+  }
+  if (node->right) {
+    node->right->rc++;
+    scanBlack(node->right);
+  }
+}
+
+void scan(Node node) {
+  printf("[scan] Node: %d, rc: %d\n", node->value, node->rc);
+  if (node->color != kGray) {
+    return;
+  }
+  if (node->rc > 0) {
+    // This node is reachable from a GC root!
+    scanBlack(node);
+    return;
+  }
+  node->color = kWhite;
+  if (node->left) {
+    scan(node->left);
+  }
+  if (node->right) {
+    scan(node->right);
+  }
+}
+
+void collectWhite(Node node) {
+  printf("[collectWhite] Node: %d, rc: %d\n", node->value, node->rc);
+  if (node->color == kWhite) {
+    node->color = kBlack; // Mark it black so that it won't be entered again
+    if (node->left) collectWhite(node->left);
+    if (node->right) collectWhite(node->right);
+    // If there were other, acyclic, references, they would have their refcounts decremented here
+    printf("[collectWhite] Freeing node %d\n", node->value);
+    free(node);
+  }
+}
+
 void decrRc(Node node)
 {
   if (node == NULL || node->rc == 0)
     return;
+  printf("[decrRc] Node: %d, rc: %d\n", node->value, node->rc);
   node->rc--;
   if (node->rc == 0)
   {
     decrRc(node->left);
     decrRc(node->right);
     free(node);
+  } else {
+    markGray(node);
+    scan(node);
+    collectWhite(node);
   }
 }
 
@@ -50,6 +125,7 @@ Node newNode(int value, Node left, Node right)
   // are set to NULL and rc is set to 0
   Node node = malloc(sizeof(struct Node_s));
   node->rc = 0;
+  node->color = kBlack;
   node->value = value;
   node->left = NULL;
   setLeft(node, left);
@@ -64,13 +140,15 @@ int example1()
   incrRc(a);
   Node b = newNode(20, a, NULL);
   incrRc(b);
-  // setRight(a, b); // We now have a cycle
+  setRight(a, b); // We now have a cycle
 
+  int res = a->value + b->value;
   decrRc(a); // The variable a goes out of scope
   decrRc(b);
+  return res;
 }
 
 int main()
 {
-  example1();
+  printf("Result: %d\n", example1());
 }
