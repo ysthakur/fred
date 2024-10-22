4af1a9559df9899674148b58823de6d977271385
ysthakur <45539777+ysthakur@users.noreply.github.com>
2024-10-22 10:55:33 -0400

    Test the contrived example

--- src/test/scala/fred/ExecTests.scala
+++ src/test/scala/fred/ExecTests.scala
@@ -70,4 +70,63 @@ class ExecTests extends munit.FunSuite with SnapshotAssertions {
     val valgrindOut = valgrindOutBuf.toString
     assert(valgrindOut.contains("ERROR SUMMARY: 0 errors"), valgrindOut)
   }
+
+  test("Contrived example") {
+    val code = """
+data Context = Context {
+  name: str,
+  mut files: FileList
+}
+
+data FileList
+  = FileNil {}
+  | FileCons {
+    ctx: Context,
+    head: File,
+    tail: FileList
+  }
+
+data File = File {
+  mut exprs: ExprList
+}
+
+data ExprList
+  = ExprNil {}
+  | ExprCons {
+    head: Expr,
+    tail: ExprList
+  }
+
+data Expr = Expr {
+  file: File
+}
+
+fn addFile(ctx: Context, file: File): int =
+  set ctx.files FileCons { ctx: ctx, head: file, tail: ctx.files };
+  0
+fn addExpr(file: File, expr: Expr): int =
+  set file.exprs ExprCons { head: expr, tail: file.exprs };
+  0
+
+fn main(): int =
+  let ctx = Context { name: "foo", files: FileNil {} } in
+  let file = File { exprs: ExprNil {} } in
+  addFile(ctx, file);
+  (let expr = Expr { file: file } in
+  addExpr(file, expr);
+  0)
+      """
+    val compiled = Compiler.compile(code, Paths.get("foo.c"), "a.out")
+
+    val valgrindOutBuf = StringBuilder()
+    assertNoDiff(
+      "",
+      "valgrind -s --leak-check=yes ./a.out" !! ProcessLogger(
+        _ => {},
+        err => valgrindOutBuf.append('\n').append(err)
+      )
+    )
+    val valgrindOut = valgrindOutBuf.toString
+    assert(valgrindOut.contains("ERROR SUMMARY: 0 errors"), valgrindOut)
+  }
 }
