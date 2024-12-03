f01dc72ed33da94231e7dd00a59ee31d5958a8e5
ysthakur <45539777+ysthakur@users.noreply.github.com>
2024-12-03 04:02:05 -0500

    test: Check reachability in each SCC

--- src/test/scala/fred/SCCTests.scala
+++ src/test/scala/fred/SCCTests.scala
@@ -7,9 +7,7 @@ import org.scalacheck.Gen
 import org.scalacheck.Shrink
 
 class SCCTests
-    extends AnyFunSuite,
-      SnapshotAssertions,
-      ScalaCheckPropertyChecks {
+    extends AnyFunSuite, SnapshotAssertions, ScalaCheckPropertyChecks {
 
   /** This is just a helper to avoid typing out calls to the TypeDef ctor */
   def createFile(graph: Map[String, Set[(Boolean, String)]]): ParsedFile = {
@@ -32,56 +30,44 @@ class SCCTests
   }
 
   test("Basic SCCs") {
-    val file = createFileImmutable(
-      Map(
-        "A" -> Set("B", "C"),
-        "B" -> Set("A"),
-        "C" -> Set("D"),
-        "D" -> Set("E"),
-        "E" -> Set("F"),
-        "F" -> Set("D")
-      )
-    )
+    val file = createFileImmutable(Map(
+      "A" -> Set("B", "C"),
+      "B" -> Set("A"),
+      "C" -> Set("D"),
+      "D" -> Set("E"),
+      "E" -> Set("F"),
+      "F" -> Set("D")
+    ))
 
     assert(
-      List(Set("B", "A"), Set("C"), Set("D", "F", "E"))
-        === simplifySCCs(Cycles.fromFile(file).sccs)
+      List(Set("B", "A"), Set("C"), Set("D", "F", "E")) ===
+        simplifySCCs(Cycles.fromFile(file).sccs)
     )
   }
 
   test("Multiple SCCs at same level") {
-    val file = createFileImmutable(
-      Map(
-        "A" -> Set("B", "C", "D", "E"),
-        "B" -> Set(),
-        "C" -> Set("G"),
-        "D" -> Set("G"),
-        "E" -> Set("F"),
-        "F" -> Set("E"),
-        "G" -> Set()
-      )
-    )
+    val file = createFileImmutable(Map(
+      "A" -> Set("B", "C", "D", "E"),
+      "B" -> Set(),
+      "C" -> Set("G"),
+      "D" -> Set("G"),
+      "E" -> Set("F"),
+      "F" -> Set("E"),
+      "G" -> Set()
+    ))
 
     assert(
-      List(
-        Set("A"),
-        Set("D"),
-        Set("C"),
-        Set("G"),
-        Set("B"),
-        Set("F", "E")
-      ) === simplifySCCs(Cycles.fromFile(file).sccs)
+      List(Set("A"), Set("D"), Set("C"), Set("G"), Set("B"), Set("F", "E")) ===
+        simplifySCCs(Cycles.fromFile(file).sccs)
     )
   }
 
   test("Is basic bad SCC detected") {
-    val file = createFile(
-      Map(
-        "A" -> Set((true, "B")),
-        "B" -> Set((false, "C")),
-        "C" -> Set((false, "A"))
-      )
-    )
+    val file = createFile(Map(
+      "A" -> Set((true, "B")),
+      "B" -> Set((false, "C")),
+      "C" -> Set((false, "A"))
+    ))
 
     val cycles = Cycles.fromFile(file)
     assert(cycles.sccs.size === 1)
@@ -89,32 +75,62 @@ class SCCTests
   }
 
   test("Is basic good SCC not incorrectly marked bad") {
-    val file = createFileImmutable(
-      Map(
-        "A" -> Set("B", "C", "D", "E"),
-        "B" -> Set(),
-        "C" -> Set("G"),
-        "D" -> Set("G"),
-        "E" -> Set("F"),
-        "F" -> Set("E"),
-        "G" -> Set("D")
-      )
-    )
+    val file = createFileImmutable(Map(
+      "A" -> Set("B", "C", "D", "E"),
+      "B" -> Set(),
+      "C" -> Set("G"),
+      "D" -> Set("G"),
+      "E" -> Set("F"),
+      "F" -> Set("E"),
+      "G" -> Set("D")
+    ))
 
     val cycles = Cycles.fromFile(file)
     assert(cycles.badSCCs.isEmpty)
   }
 
   def validateSccs(file: ParsedFile, cycles: Cycles) = {
+    val allTypes = cycles.sccs.flatten
+    assert(allTypes.distinct === allTypes)
+    assert(allTypes.toSet == file.typeDefs.toSet)
+
     val bindings = Bindings.fromFile(file)
+
+    // Ensure that SCCs are sorted
     for {
       typ <- file.typeDefs
       field <- typ.cases.head.fields
     } do {
       bindings.getType(field.typ) match {
-        case td: TypeDef =>
-          assert(cycles.sccMap(typ) <= cycles.sccMap(td))
-        case _ => {}
+        case td: TypeDef => assert(cycles.sccMap(typ) <= cycles.sccMap(td))
+        case _           =>
+      }
+    }
+
+    // All types in the same SCC should be able to reach other
+    for {
+      scc <- cycles.sccs
+      from <- scc
+      to <- scc
+    } do { assert(reachable(from, to, bindings, Set.empty)) }
+  }
+
+  def reachable(
+      from: TypeDef,
+      to: TypeDef,
+      bindings: Bindings,
+      prev: Set[TypeDef]
+  ): Boolean = {
+    if (from == to) true
+    else if (prev(from)) false
+    else {
+      val neighbors = from.cases.flatMap(_.fields).map(_.typ.name)
+      if (neighbors.contains(to.name)) true
+      else neighbors.exists { name =>
+        bindings.types(name) match {
+          case td: TypeDef => reachable(td, to, bindings, prev + from)
+          case _           => false
+        }
       }
     }
   }
